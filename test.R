65edd51b59393ae1c53acb9aa22505a49fd9370f
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2022-03-18 17:03:39 +0000

    rework example tests

--- test.R
+++ test.R
@@ -1,2 +1,8 @@
 library(testthat)
-testthat::test_dir("tests")
+args <- commandArgs()
+
+if ("--example" %in% args || "--test" %in% args) {
+  testthat::test_file("tests/example/test-exercises_000.R")
+} else {
+  testthat::test_dir("tests")
+}
